48c9b599a639ba767e771f68434701be32d447e7
Jon Staab <shtaab@gmail.com>
2015-06-30 20:06:29 -0700

    Adding time to chatterbot

--- chatterbot/__main__.py
+++ chatterbot/__main__.py
@@ -1,6 +1,8 @@
 import chatterbot
 import random
 
+from timekeeper import TimeKeeper
+
 print "Say something to our chatterbot!"
 
 bots = [
@@ -9,6 +11,10 @@ bots = [
     chatterbot.Bot("Brunhilde")
 ]
 
+keeper = TimeKeeper()
+keeper.start()
+
 while True:
     inp = raw_input()
     print random.choice(bots).chat(inp)
+    print "Time elapsed: %d seconds" % int(keeper.elapsed())
---
+++ chatterbot/timekeeper.py
@@ -0,0 +1,16 @@
+import time
+
+
+class TimeKeeper(object):
+    def __init__(self):
+        self.start_time = None
+        self.end_time = None
+
+    def start(self):
+        self.start_time = time.time()
+
+    def stop(self):
+        self.end_time = time.time()
+
+    def elapsed(self):
+        return time.time() - self.start_time
