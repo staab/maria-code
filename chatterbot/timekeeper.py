ebaa85aad9f5e9bda1b682ca26a6322fe3fdeb15
Maria Staab <maria@marianetbook.(none)>
2015-06-30 20:27:46 -0700

    pause

--- chatterbot/timekeeper.py
+++ chatterbot/timekeeper.py
@@ -5,12 +5,24 @@ class TimeKeeper(object):
     def __init__(self):
         self.start_time = None
         self.end_time = None
+        self.pause_time = None
+        self.time_paused = 0
+
+    def now(self):
+    	if self.pause_time:
+    		return self.pause_time
+    	else: 
+    		return time.time()
 
     def start(self):
-        self.start_time = time.time()
+        self.start_time = self.now()
 
-    def stop(self):
-        self.end_time = time.time()
+    def pause (self):
+    	self.pause_time = self.now()
+
+    def resume (self):
+    	self.time_paused += time.time() - self.pause_time
+    	self.pause_time = None
 
     def elapsed(self):
-        return time.time() - self.start_time
+        return self.now() - self.start_time - self.time_paused
